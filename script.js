817e3e4f316373b55f024486570481e6e512e299
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-02-04 11:54:16 -0800

    computation

--- script.js
+++ script.js
@@ -31,7 +31,29 @@ class Calculator {
     }
 
     compute() {
-
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break;
+            case '-':
+                computation = prev - current
+                break;
+            case '*':
+                computation = prev * current
+                break;
+            case '÷':
+                computation = prev / current
+                break;   
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
 
     updateDisplay() {
@@ -64,3 +86,8 @@ operationButtons.forEach(button => {
         calculator.updateDisplay();
     })
 })
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
